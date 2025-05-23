cda79a9b0f3c2e3fb9e59dc891144d1cdda9694f
Idhibhat Pankam <marginpankam@gmail.com>
2025-05-23 23:07:00 +0700

    refactor

--- src/router/router.cpp
+++ src/router/router.cpp
@@ -5,24 +5,29 @@
 #include <iostream>
 #include <map>
 
+const std::string HTTP_OK = "HTTP/1.1 200 OK\r\n";
+const std::string HTTP_NOT_FOUND = "HTTP/1.1 404 Not Found\r\n";
+const std::string CONTENT_TEXT = "Content-Type: text/plain\r\n";
+const std::string CONTENT_LENGTH = "Content-Length: ";
+
 std::string handleRequest(const std::string &method, const std::string &path, const std::map<std::string, std::string> &headers)
 {
     if (method == "GET" && path == "/")
     {
-        return "HTTP/1.1 200 OK\r\n\r\n";
+        return HTTP_OK + "\r\n";
     }
 
     const std::vector<std::string> pathParts = splitString(path, "/");
     if (pathParts.size() > 1 && pathParts[1] == "echo")
     {
-        int contentLength = pathParts[2].size();
-        return std::format("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}", contentLength, pathParts[2]);
+        std::string contentLength = CONTENT_LENGTH + std::to_string(pathParts[2].size());
+        return std::format("{}{}{}\r\n\r\n{}", HTTP_OK, CONTENT_TEXT, contentLength, pathParts[2]);
     }
     else if (pathParts.size() > 1 && pathParts[1] == "user-agent")
     {
-        int contentLength = headers.at("User-Agent").size();
-        return std::format("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}", contentLength, headers.at("User-Agent"));
+        std::string contentLength = CONTENT_LENGTH + std::to_string(headers.at("User-Agent").size());
+        return std::format("{}{}{}\r\n\r\n{}", HTTP_OK, CONTENT_TEXT, contentLength, headers.at("User-Agent"));
     }
 
-    return "HTTP/1.1 404 Not Found\r\n\r\n";
+    return HTTP_NOT_FOUND + "\r\n";
 }
